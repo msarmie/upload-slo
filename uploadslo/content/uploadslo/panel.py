181aaf5cd7a4ff4eb2fcf964b3acc68e7475f27c
Mohana Sarmiento <mo.sarmiento@utoronto.ca>
2021-07-09 12:19:44 -0400

    Update Large Objects panel as blank page

--- uploadslo/content/uploadslo/panel.py
+++ uploadslo/content/uploadslo/panel.py
@@ -2,5 +2,5 @@ from django.utils.translation import ugettext_lazy as _
 import horizon
 
 class UploadSlo(horizon.Panel):
-    name = _("Upload SLO")
+    name = _("Large Objects")
     slug = "uploadslo"
